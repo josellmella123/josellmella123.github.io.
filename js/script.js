cf6e9e0dd91a607e289b5a6016ca90d62c498f84
josellmella123 <jpllmella@gmail.com>
2024-10-07 22:49:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ function closeGameOverlay() {
 
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('/js/service-worker.js')
   .then(registration => {
     console.log('Service Worker registrado con éxito:', registration);
   }).catch(error => {
